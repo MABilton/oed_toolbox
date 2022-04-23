f5c91110cd24755a48fefe670e3b2fb0f095475e
MABilton <Matt.A.Bilton@gmail.com>
2022-04-23 20:20:44 +1200

    Added to use reparameterisation + control variates.

--- oed_toolbox/losses.py
+++ oed_toolbox/losses.py
@@ -7,13 +7,13 @@ from . import distributions, utils
 
 class APE:
 
-    def __init__(self, prior, likelihood, posterior, apply_control_variates=True, use_reparameterisation=False):
+    def __init__(self, prior, likelihood, posterior, use_reparameterisation=False):
         if use_reparameterisation:
             self._loss_and_grad = self._create_reparameterisation_loss(prior, likelihood, posterior)
         else:
-            self._loss_and_grad = self._create_loss(prior, likelihood, posterior, apply_control_variates)
+            self._loss_and_grad = self._create_loss(prior, likelihood, posterior)
 
-    def __call__(self, d, num_samples=None, samples=None, rng=None, return_grad=True):
+    def __call__(self, d, num_samples=None, samples=None, rng=None, apply_control_variates=True, return_grad=True):
         if (num_samples is None) and (samples is None):
             raise ValueError('Must specify either num_samples or samples as an input.')
         if num_samples is None:
@@ -21,19 +21,19 @@ class APE:
             num_samples = list(samples.values())[0].shape[0]
         if samples is None:
             samples = {}
-        return self._loss_and_grad(d, num_samples, samples, rng, return_grad)
+        return self._loss_and_grad(d, num_samples, samples, rng, apply_control_variates, return_grad)
 
     @classmethod
-    def using_laplace_approximation(cls, model, minimizer, prior_mean, prior_cov, noise_cov, apply_control_variates=True, use_reparameterisation=False):
+    def using_laplace_approximation(cls, model, minimizer, prior_mean, prior_cov, noise_cov, use_reparameterisation=False):
         prior = distributions.Prior.gaussian(prior_mean, prior_cov)
         likelihood = distributions.Likelihood.from_model_plus_constant_gaussian_noise(model, noise_cov)
         approx_posterior = distributions.Posterior.laplace_approximation(model, minimizer, noise_cov, prior_mean, prior_cov)
-        return cls(prior, likelihood, approx_posterior, apply_control_variates, use_reparameterisation)
+        return cls(prior, likelihood, approx_posterior, use_reparameterisation)
 
-    @staticmethod
-    def _create_reparameterisation_loss(prior, likelihood, posterior):
+    def _create_reparameterisation_loss(self, prior, likelihood, posterior):
         
-        def ape_and_grad(d, num_samples, samples, rng, return_grad):
+        def ape_and_grad(d, num_samples, samples, rng, apply_control_variates, return_grad):
+            outputs = {}
             if 'theta' in samples:
                 theta = samples['theta']
             else:
@@ -44,20 +44,20 @@ class APE:
                 epsilon = likelihood.sample_base(num_samples, rng)
             transform = likelihood.transform(epsilon, theta, d, return_dd=return_grad)
             post_vals = posterior.logpdf(theta, transform['y'], d, return_dd=return_grad, return_dy=return_grad)
-            loss_samples = post_vals['logpdf']
+            outputs['loss'] = post_vals['logpdf']
             if return_grad:
-                grad_samples = np.einsum('aij,ai->aj', transform['y_dd'], post_vals['logpdf_dy']) + post_vals['logpdf_dd']
-            loss = -1*np.mean(loss_samples)
-            if return_grad:
-                loss_del_d = -1*np.mean(grad_samples, axis=0)
-            return loss if not return_grad else (loss, loss_del_d)
+                outputs['loss_del_d'] = \
+                np.einsum('aij,ai->aj', transform['y_dd'], post_vals['logpdf_dy']) + post_vals['logpdf_dd']
+            if apply_control_variates:
+                like_grad = likelihood.logpdf(transform['y'], theta, d, return_logpdf=False, return_dd=True)['logpdf_dd']
+            outputs = self._average_samples(outputs, like_grad, apply_control_variates)
+            return outputs['loss'] if not return_grad else (outputs['loss'], outputs['loss_del_d'])
 
         return ape_and_grad
 
-    @staticmethod
-    def _create_loss(prior, likelihood, posterior, apply_control_variates):
+    def _create_loss(self, prior, likelihood, posterior):
         
-        def ape_and_grad(d, num_samples, samples, rng, return_grad):
+        def ape_and_grad(d, num_samples, samples, rng, apply_control_variates, return_grad):
             outputs = {}
             if 'theta' in samples:
                 theta = samples['theta']
@@ -74,15 +74,19 @@ class APE:
                 like_grad = likelihood.logpdf(y, theta, d, return_logpdf=False, return_dd=True)['logpdf_dd']
             if return_grad:
                 outputs['loss_del_d'] = np.einsum('a,ai->ai', post_vals['logpdf'], like_grad) + post_vals['logpdf_dd']
-            for key, val in outputs.items():
-                if apply_control_variates:
-                    outputs[key] = -1*utils.apply_control_variates(val, cv=like_grad)
-                else:
-                    outputs[key] = -1*np.mean(val, axis=0)
+            outputs = self._average_samples(outputs, like_grad, apply_control_variates)
             return outputs['loss'] if not return_grad else (outputs['loss'], outputs['loss_del_d'])
 
         return ape_and_grad
 
+    @staticmethod
+    def _average_samples(outputs, like_grad, apply_control_variates):
+        for key, val in outputs.items():
+            if apply_control_variates:
+                outputs[key] = -1*utils.apply_control_variates(val, cv=like_grad)
+            else:
+                outputs[key] = -1*np.mean(val, axis=0)
+        return outputs
 #
 #   'Alphabet' Optimal Criteria
 #
